c9b7f809082822d88b9de98ca625b2e2fcc76c89
Nicolas Canac <ncanac@gmail.com>
2015-09-04 20:32:14 -0700

    added simple binary regression exercise with linear decision boundary

---
+++ linear_regression/lin_reg.py
@@ -0,0 +1,29 @@
+import numpy as np
+from sklearn import linear_model
+from matplotlib import pyplot as plt
+
+def main():
+    # Read in data:
+    data = np.loadtxt('ex1data1.txt')
+    m = len(data)
+    X = np.ones((m, 2))
+    X[:, 1] = data[:, 0] # population size
+    y = data[:, 1] # profit
+
+    # Run linear regression:
+    lr = linear_model.LinearRegression()
+    lr.fit(X, y)
+
+    # Predict profits for populations of 35,000 and 70,000
+    predict = lr.predict([1, 3.5])[0]
+    print "For population = 35000, we predict a profit of ${:.2f}.".format(predict*10000)
+    predict = lr.predict([1, 7])[0]
+    print "For population = 70000, we predict a profit of ${:.2f}.".format(predict*10000)
+
+    # Plot data and model
+    plt.scatter(X[:, 1], y)
+    plt.plot(X[:, 1], lr.predict(X))
+    plt.show()
+
+if __name__ == "__main__":
+    main()
---
+++ logistic_regression/binary/log_reg.py
@@ -0,0 +1,43 @@
+"""
+
+This script implements binary logistic regression to the classification
+problem in exercise 2 of Andrew Ng's machine learning course on Coursera.
+
+"""
+
+import numpy as np
+from numpy.polynomial.polynomial import polyval
+import matplotlib.pyplot as plt
+from sklearn import linear_model as lm
+
+# Load the data from ex2data1.txt into X and y
+data = np.loadtxt("ex2data1.txt")
+X = data[:, 0:2]
+y = data[:,2]
+
+# Create a LogisticRegression object
+logreg = lm.LogisticRegression(penalty='l2', dual=False, C=1, solver='lbfgs')
+
+# Fit the data
+logreg.fit(X, y)
+
+# Plot decision boundary
+x1min, x1max = np.min(X[:, 0]) - 10, np.max(X[:, 0]) + 10
+x2min, x2max = np.min(X[:, 1]) - 10, np.max(X[:, 1]) + 10 
+d = 0.1
+xx, yy = np.mgrid[x1min:x1max:d, x2min:x2max:d]
+probs = logreg.predict(np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
+plt.figure(1, figsize=(8, 6))
+plt.pcolormesh(xx, yy, probs, cmap="RdBu")
+
+# Plot the data, positive values in blue and negative values in red
+plt.scatter(X[:,0], X[:, 1], c=y, s=50, cmap="RdBu", vmin=-.2, vmax=1.2, edgecolor="white", linewidth=1)
+
+plt.xlim(x1min, x1max)
+plt.ylim(x2min, x2max)
+plt.xlabel("$x_1$", size=20)
+plt.ylabel("$x_2$", size=20)
+plt.tight_layout()
+
+plt.show()
+#plt.savefig("log_reg_plot1.png")
